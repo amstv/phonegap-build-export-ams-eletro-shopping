36b954483b780f2cb10a88c4550d771945de8548
Anderson Marcos dos Santos <amstv@ig.com.br>
2018-04-14 16:38:15 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'ams-eletro-shopping',
+		wp_ws_url : 'http://amseletroshopping.amstva.com/wp-appkit-api/ams-eletro-shopping',
+		wp_url : 'http://amseletroshopping.amstva.com',
+		theme : 'q-android',
+		version : '1.0.1',
+		app_type : 'phonegap-build',
+		app_title : 'Eletro Shopping',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : -4,
+		debug_mode : 'off',
+		auth_key : 'PvK5P/UKPxk{R!II!!q{^wH$Mn2(^t.~K@nsx]-jepLYW y!a5){C2)&$-F>~>ox',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
